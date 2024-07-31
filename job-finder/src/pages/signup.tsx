d48fe7c02918cadb8ff2c5582bc8f88b283b1672
orkun.polat <orkun.polat001@gmail.com>
2024-07-31 13:24:43 +0300

    UI of the modal signup.tsx is developed

---
+++ job-finder/src/pages/signup.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface SignupProps {
+  onClose: () => void;
+  onSwitchToLogin: () => void;
+}
+
+const Signup: React.FC<SignupProps> = ({ onClose, onSwitchToLogin }) => {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center z-50">
+      <div className="bg-white p-8 rounded shadow-lg max-w-md w-full relative lg:mb-36 mb-64">
+        <button className="absolute top-0 right-0 mt-4 mr-4" onClick={onClose}>
+          &times;
+        </button>
+        <h2 className="text-2xl font-bold mb-4 mt-2 flex justify-center">SIGN UP</h2>
+        <form>
+          <div className="mb-8">
+            <label className="block text-sm font-medium mb-2" htmlFor="email">Email</label>
+            <input className="border px-4 py-2 w-full border-1 border-black" type="email" id="email" name="email" />
+          </div>
+          <div className="mb-8">
+            <label className="block text-sm font-medium mb-2" htmlFor="password">Password</label>
+            <input className="border px-4 py-2 w-full border-1 border-black" type="password" id="password" name="password" />
+          </div>
+          <div className='flex justify-center'>
+          <button className="bg-white text-black border-2 border-black shadow-[4px_4px_0px_0px_#1a202c] px-24 py-2 rounded" type="submit">Sign Up</button>
+          </div>
+        </form>
+        <p className="mt-6 text-center">
+          Already have an account? <span className="text-blue-700 cursor-pointer" onClick={onSwitchToLogin}>Login</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
