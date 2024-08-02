53193f3d27d37df3d2a063e010027c4ef26cc1d8
orkun.polat <orkun.polat001@gmail.com>
2024-08-02 12:07:12 +0300

    the other pages has developed

---
+++ job-finder/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Homepage from "@/pages/homepage";
+import "./globals.css";
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Homepage></Homepage>
+    </div>
+   
+  );
+}
